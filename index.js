0040bce51d4698aa27727a9d098918bac693eb5a
Jan Buschtöns <jan@buschtoens.me>
2020-08-10 23:39:21 +0200

    feat: load polyfill

--- index.js
+++ index.js
@@ -2,4 +2,41 @@
 
 module.exports = {
   name: require("./package").name,
+
+  included() {
+    this._super.included.apply(this, arguments);
+    this._ensureThisImport();
+
+    this.import("vendor/ember-memo-decorator-polyfill/index.js");
+  },
+
+  treeForVendor(tree) {
+    const babel = this.addons.find((a) => a.name === "ember-cli-babel");
+
+    return babel.transpileTree(tree, {
+      babel: this.options.babel,
+
+      "ember-cli-babel": {
+        compileModules: false,
+      },
+    });
+  },
+
+  _ensureThisImport() {
+    if (!this.import) {
+      this._findHost = function findHostShim() {
+        let current = this;
+        let app;
+        do {
+          app = current.app || app;
+          // eslint-disable-next-line no-cond-assign
+        } while (current.parent.parent && (current = current.parent));
+        return app;
+      };
+      this.import = function importShim(asset, options) {
+        const app = this._findHost();
+        app.import(asset, options);
+      };
+    }
+  },
 };
---
+++ vendor/ember-memo-decorator-polyfill/index.js
@@ -0,0 +1,41 @@
+import Ember from "ember";
+import { assert } from "@ember/debug";
+
+(() => {
+  "use strict";
+
+  const {
+    _createCache: createCache,
+    _cacheGetValue: getValue,
+    _cacheIsConst: isConst,
+  } = Ember;
+
+  function memo(...args) {
+    const [target, key, descriptor] = args;
+
+    // Error on `@memo()`, `@memo(...args)`, and `@memo propName = value;`
+    assert(
+      "You attempted to use @memo(), which is not necessary nor supported. Remove the parentheses and you will be good to go!",
+      target !== undefined
+    );
+    assert(
+      `You attempted to use @memo on with ${
+        args.length > 1 ? "arguments" : "an argument"
+      } ( @memo(${args
+        .map((d) => `'${d}'`)
+        .join(
+          ", "
+        )}), which is not supported. Dependencies are automatically tracked, so you can just use ${"`@memo`"}`,
+      typeof target === "object" &&
+        typeof key === "string" &&
+        typeof descriptor === "object" &&
+        args.length === 3
+    );
+    assert(
+      `The @memo decorator must be applied to getters. '${key}' is not a getter.`,
+      typeof descriptor.get == "function"
+    );
+  }
+
+  Ember._memo = memo;
+})();
