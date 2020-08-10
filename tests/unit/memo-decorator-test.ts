8da267ad3a1aaf067f036b8240f5f777de80809b
Jan Buschtöns <jan@buschtoens.me>
2020-08-10 23:04:42 +0200

    test: add basic test case

---
+++ tests/unit/memo-decorator-test.ts
@@ -0,0 +1,42 @@
+import { module, test } from 'qunit';
+import { tracked, memo } from '@glimmer/tracking';
+
+module('Unit | Decorators | @memo', function() {
+  test('it works', function(assert) {
+    class Person {
+      @tracked firstName = 'Jen';
+      @tracked lastName = 'Weber';
+
+      @memo
+      get fullName() {
+        const fullName = `${this.firstName} ${this.lastName}`;
+        assert.step(fullName);
+        return fullName;
+      }
+    }
+
+    const person = new Person;
+    assert.verifySteps([], 'getter is not called after class initialization');
+
+    assert.strictEqual(person.fullName, 'Jen Weber');
+    assert.verifySteps(['Jen Weber'], 'getter was called after property access');
+
+    assert.strictEqual(person.fullName, 'Jen Weber');
+    assert.verifySteps([], 'getter was not called again after repeated property access');
+
+    person.firstName = 'Kenneth';
+    assert.verifySteps([], 'changing a property does not trigger an eager re-computation');
+
+    assert.strictEqual(person.fullName, 'Kenneth Weber');
+    assert.verifySteps(['Kenneth Weber'], 'accessing the property triggers a re-computation');
+
+    assert.strictEqual(person.fullName, 'Kenneth Weber');
+    assert.verifySteps([], 'getter was not called again after repeated property access');
+
+    person.lastName = 'Larsen';
+    assert.verifySteps([], 'changing a property does not trigger an eager re-computation');
+
+    assert.strictEqual(person.fullName, 'Kenneth Larsen');
+    assert.verifySteps(['Kenneth Larsen'], 'accessing the property triggers a re-computation');
+  });
+});
