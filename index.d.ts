bb7e97dc8aae0d2ad018fad7cf92535ce0c4561b
Jan Buschtöns <jan@buschtoens.me>
2020-08-10 22:59:03 +0200

    docs(types): add docs to types

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+declare module '@glimmer/tracking' {
+  /**
+   * @decorator
+   *
+   * Memoizes the result of a getter based on autotracking.
+   *
+   * The `@memo` decorator can be used on native getters to memoize their return
+   * values based on the tracked state they consume while being calculated.
+   *
+   * By default a getter is always re-computed every time it is accessed. On
+   * average this is faster than caching every getter result by default.
+   *
+   * However, there are absolutely cases where getters are expensive, and their
+   * values are used repeatedly, so memoization would be very helpful.
+   * Strategic, opt-in memoization is a useful tool that helps developers
+   * optimize their apps when relevant, without adding extra overhead unless
+   * necessary.
+   *
+   * @example
+   *
+   * ```ts
+   * import { tracked, memo } from '@glimmer/tracking';
+   *
+   * class Person {
+   *   @tracked firstName = 'Jen';
+   *   @tracked lastName = 'Weber';
+   *
+   *   @memo
+   *   get fullName() {
+   *     return `${this.firstName} ${this.lastName}`;
+   *   }
+   * }
+   * ```
+   */
+  export let memo: PropertyDecorator;
+
+  export { tracked, setPropertyDidChange } from '@glimmer/tracking/dist/types/src/tracked';
+}
