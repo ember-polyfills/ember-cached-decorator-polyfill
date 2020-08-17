eeeaf9afad4beb4d09c2b60ff0f2a73f3e8edbf2
Francesco Novy <francesco@fabscale.com>
2020-08-17 11:36:33 +0200

    chore(test): Add integration test

---
+++ tests/integration/cached-decorator-test.ts
@@ -0,0 +1,53 @@
+import { module, test } from 'qunit';
+import { tracked, cached } from '@glimmer/tracking';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, settled } from '@ember/test-helpers';
+import { TestContext } from 'ember-test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+type Context = TestContext & {
+  personA: any;
+  personB: any;
+};
+
+module('Integration | Decorators | @cached', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it works', async function (this: Context, assert) {
+    class Person {
+      @tracked firstName = '';
+      @tracked lastName = '';
+
+      constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+      }
+
+      @cached
+      get fullName() {
+        const fullName = `${this.firstName} ${this.lastName}`;
+        assert.step(fullName);
+        return fullName;
+      }
+    }
+
+    this.personA = new Person('Jen', 'Weber');
+    this.personB = new Person('Mel', 'Sumner');
+
+    await render(hbs`
+      <div data-person-a>{{this.personA.fullName}}</div>
+      <div data-person-B>{{this.personB.fullName}}</div>
+    `);
+
+    assert.dom('[data-person-a]').hasText('Jen Weber');
+    assert.dom('[data-person-b]').hasText('Mel Sumner');
+
+    this.personA.firstName = 'Bob';
+    await settled();
+
+    assert.dom('[data-person-a]').hasText('Bob Weber');
+    assert.dom('[data-person-b]').hasText('Mel Sumner');
+
+    assert.verifySteps(['Jen Weber', 'Mel Sumner', 'Bob Weber']);
+  });
+});
