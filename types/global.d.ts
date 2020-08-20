2959dbc497def76b1789465c7844d9c13cf2049b
Jan Buschtöns <jan@buschtoens.me>
2020-08-20 18:20:54 +0200

    chore: remove some superfluous directories

--- types/global.d.ts
+++
@@ -1,6 +0,0 @@
-// Types for compiled templates
-declare module 'ember-cached-decorator-polyfill/templates/*' {
-  import { TemplateFactory } from 'htmlbars-inline-precompile';
-  const tmpl: TemplateFactory;
-  export default tmpl;
-}
