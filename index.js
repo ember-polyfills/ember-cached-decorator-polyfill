d13c745e257e1361a874adfebf3ba03b6bb48284
Francesco Novy <francesco@fabscale.com>
2021-05-12 18:03:15 +0200

    refactor: Use babel AST for polyfill

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 'use strict';
 
-const ReplaceImportsPreprocessor = require('./lib/replace-imports-preprocessor');
+const { resolve } = require('path');
 
 module.exports = {
   name: require('./package').name,
 
-  setupPreprocessorRegistry(type, registry) {
-    if (type !== 'parent') {
-      return;
-    }
+  included() {
+    this._super.included.apply(this, arguments);
 
-    registry.add('js', new ReplaceImportsPreprocessor());
+    this.patchEmberModulesAPIPolyfill();
+  },
+
+  patchEmberModulesAPIPolyfill() {
+    const babel = this.parent.findOwnAddonByName
+      ? this.parent.findOwnAddonByName('ember-cli-babel') // parent is an addon
+      : this.parent.findAddonByName('ember-cli-babel'); // parent is an app
+
+    if (babel.__CachedDecoratorPolyfillApplied) return;
+    babel.__CachedDecoratorPolyfillApplied = true;
+
+    const { _getEmberModulesAPIPolyfill } = babel;
+
+    babel._getEmberModulesAPIPolyfill = function (...args) {
+      const plugins = _getEmberModulesAPIPolyfill.apply(this, args);
+      if (!plugins) return;
+
+      return [[resolve(__dirname, './lib/transpile-modules.js')], ...plugins];
+    };
   }
 };
