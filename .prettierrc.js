f2160d8f7658cfb0626c6a59260c35739c28a9f7
Edward Faulkner <edward@eaf4.com>
2022-09-15 17:04:45 -0400

    Updating to blueprint for ember 4.7

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,5 @@
 'use strict';
 
 module.exports = {
-  arrowParens: 'avoid',
   singleQuote: true,
-  trailingComma: 'none' // @TODO: switch to 'all'
 };
--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
@@ -10,58 +11,76 @@ module.exports = async function () {
         name: 'ember-3.13',
         npm: {
           devDependencies: {
-            'ember-source': '~3.13.0'
-          }
-        }
+            'ember-source': '~3.13.0',
+          },
+        },
       },
       {
         name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
-            'ember-source': '~3.16.0'
-          }
-        }
+            'ember-source': '~3.16.0',
+          },
+        },
       },
       {
         name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
-            'ember-source': '~3.20.0'
-          }
-        }
+            'ember-source': '~3.20.0',
+          },
+        },
       },
       {
         name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~3.24.0'
-          }
-        }
+            'ember-source': '~3.24.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
+          },
+        },
       },
       {
         name: 'ember-release',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
+            'ember-source': await getChannelURL('release'),
+          },
+        },
       },
       {
         name: 'ember-beta',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
+            'ember-source': await getChannelURL('beta'),
+          },
+        },
       },
       {
         name: 'ember-canary',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
-      }
-    ]
+            'ember-source': await getChannelURL('canary'),
+          },
+        },
+      },
+      embroiderSafe(),
+      embroiderOptimized(),
+    ],
   };
 };
